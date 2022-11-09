bb36aa6146edb9787a1bf99a68bc01a2a967f287
Jasmin Köhler <jasminangelique@gmail.com>
2022-11-09 08:38:15 +0100

    optimised canvas

--- public/canvas.js
+++ public/canvas.js
@@ -1,56 +1,56 @@
 const canvas = document.querySelector("#signature-field");
 const ctx = canvas.getContext("2d");
 const hiddenInput = document.querySelector("#sig");
-// console.log(hiddenInput);
-// console.log("canvas datas", signatureData);
-
-const offsetX = canvas.offsetLeft;
-const offsetY = canvas.offsetTop;
-
-// console.log(offsetX);
-// console.log(offsetY);
 
 canvas.width = 300;
-canvas.height = 50;
+canvas.height = 100;
 ctx.strokeStyle = "#fff";
 ctx.lineWidth = 2;
 
 let signing = false;
-let mouseX = 0;
-let mouseY = 0;
 
-// console.log(mouseX);
+let mousePosition = {
+    x: 0,
+    y: 0,
+};
+let currentPosition = mousePosition;
+
 canvas.addEventListener("mousedown", (event) => {
     signing = true;
-    mouseX = event.clientX - offsetX;
-    mouseY = event.clientY - offsetY;
-    // console.log("mousedown", event);
+    currentPosition = getXY(canvas, event);
+    console.log("currentPosition", currentPosition);
 });
 
 canvas.addEventListener("mouseup", () => {
     signing = false;
-    // console.log("mouseup", event);
     let signatureData = canvas.toDataURL();
-    // console.log(signatureData);
     hiddenInput.value = signatureData;
 });
 
 canvas.addEventListener("mousemove", (event) => {
     if (signing) {
-        // console.log("mouse is moving", event);
-        renderSign(event.clientX - offsetX, event.clientY - offsetY);
+        mousePosition = getXY(canvas, event);
+        renderSign(currentPosition.x, currentPosition.y);
     }
 });
 
 function renderSign(x, y) {
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(mouseX, mouseY);
-    mouseX = x;
-    mouseY = y;
+    ctx.moveTo(mousePosition.x, mousePosition.y);
+    ctx.lineTo(x, y);
+    currentPosition = mousePosition;
     ctx.stroke();
 }
-// RESET signature
+
+function getXY(canvas, event) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+    };
+}
+
+// RESET Canvas
 const reset = document.querySelector("#reset");
 
 reset.addEventListener("click", () => {
